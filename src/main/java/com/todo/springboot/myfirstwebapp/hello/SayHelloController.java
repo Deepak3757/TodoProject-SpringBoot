f3a125efc049b675037248bfa8e1ee0f73be05de
Deepak gupta <deepakgupta70363@gmail.com>
2024-10-29 09:06:32 +0530

    Added JSP (removed @ResponseBody but still code not redirecting to sayHello JSP page)

--- src/main/java/com/todo/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/todo/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -2,23 +2,11 @@ package com.todo.springboot.myfirstwebapp.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-record MyClass(int rollno, String name) {}
 
 @Controller
 public class SayHelloController {
 	@RequestMapping("say-hello")
-	@ResponseBody
 	public String sayHello() {
-		return """
-				
-				<html>
-					<head><title>The project</title></head>
-					<body style='background-color: black'>
-						<h1 style='color: red'>Hellooo World!</h2>
-					</body>
-				</html>
-				""";
+		return "sayHello";
 	}
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,6 @@
+<html>
+<head><title>The project</title></head>
+<body style='background-color: black'>
+	<h1 style='color: red'>Hellooo World!</h1>
+	</body>
+</html>
