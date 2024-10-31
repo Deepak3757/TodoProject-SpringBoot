16015e0f3ec6a0bdb23c76320a76beb4a8d35a15
Deepak gupta <deepakgupta70363@gmail.com>
2024-10-31 13:18:19 +0530

    Added Todo adding functionality but empty description submission problem yet to be tackled

--- src/main/java/com/todo/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/todo/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,9 +1,12 @@
 package com.todo.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -28,7 +31,11 @@ public class TodoController {
 		return "todo";
 	}
 	@RequestMapping(value="add-todo", method=RequestMethod.POST)
-	public String addNewTodoPage() {	
+	public String addNewTodoPage(@RequestParam String description, ModelMap model) {	
+		
+		String username=(String)model.get("name");
+		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+		
 		return "redirect:list-todos";
 	}
 	
--- src/main/java/com/todo/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/todo/springboot/myfirstwebapp/todo/TodoService.java
@@ -4,24 +4,26 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
 
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
-	
+
 	private static List<Todo> todos;
+	private static int todosCount = 0;
 	static {
-		todos=new ArrayList<Todo>();
-		todos.add(new Todo(1, "deepak", "Learn coding", LocalDate.now().plusYears(1)
-				, false));
-		todos.add(new Todo(1, "deepak", "Learn React", LocalDate.now().plusYears(2)
-				, false));
-		todos.add(new Todo(1, "deepak", "Learn SpringBoot", LocalDate.now().plusYears(3)
-				, false));
+		todos = new ArrayList<Todo>();
+		todos.add(new Todo(++todosCount, "deepak", "Learn coding", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "deepak", "Learn React", LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(++todosCount, "deepak", "Learn SpringBoot", LocalDate.now().plusYears(3), false));
 	}
-	
-	public List<Todo> findByUsername(String username){
+
+	public List<Todo> findByUsername(String username) {
 		return todos;
+	}
+
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo=new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
 	}
 }
