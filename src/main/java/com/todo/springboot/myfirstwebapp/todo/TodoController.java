93ae3dd5507d7e96f2dbaadf8d5bd9c8c7ca6a6d
Deepak gupta <deepakgupta70363@gmail.com>
2024-10-31 12:32:55 +0530

    Moving to add Add Todo functionality

--- src/main/java/com/todo/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/todo/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.todo.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -17,9 +18,19 @@ public class TodoController {
 	}
 
 	@RequestMapping("list-todos")
-	public String listAllTodos(ModelMap model) {
-		
+	public String listAllTodos(ModelMap model) {	
 		model.put("todos", todoService.findByUsername("deepak"));
 		return "listTodos";
 	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.GET)
+	public String showNewTodoPage() {	
+		return "todo";
+	}
+	@RequestMapping(value="add-todo", method=RequestMethod.POST)
+	public String addNewTodoPage() {	
+		return "redirect:list-todos";
+	}
+	
+	
 }
